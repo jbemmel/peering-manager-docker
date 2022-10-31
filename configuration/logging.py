158177117339a896764502f437214fb3cad4153e
Jeroen van Bemmel <jeroen.van_bemmel@nokia.com>
2022-10-31 15:58:35 -0500

    Extend logging config

--- configuration/logging.py
+++ configuration/logging.py
@@ -4,7 +4,7 @@
 
 from os import environ
 ## Set LOGLEVEL in peering-manager.env or docker-compose.overide.yml to override a logging level of INFO.
-LOGLEVEL = environ.get('LOGLEVEL', 'DEBUG')
+LOGLEVEL = 'DEBUG' # environ.get('LOGLEVEL', 'DEBUG')
 
 LOGGING = {
     'version': 1,
@@ -27,7 +27,7 @@ LOGGING = {
     'handlers': {
         'console': {
             'level': LOGLEVEL,
-            'filters': ['require_debug_false'],
+            # 'filters': ['require_debug_false'],
             'class': 'logging.StreamHandler',
             'formatter': 'simple'
         },
@@ -38,6 +38,22 @@ LOGGING = {
         }
     },
     'loggers': {
+        'logging.root': {
+            'handlers': ['console'],
+            'level': 'DEBUG'
+        },
+        'napalm.sros': {
+            'handlers': ['console'],
+            'level': 'DEBUG'
+        },
+        'peering.manager.peeringdb': {
+            'handlers': ['console'],
+            'level': 'DEBUG'
+        },
+        'peering.manager.napalm': {
+            'handlers': ['console'],
+            'level': 'DEBUG'
+        },
         'django': {
             'handlers': ['console'],
             'propagate': True,
